e0bb616dc121bdad2d827563d139663f51ca35cf
Vishesh Bardia <developvishesh@gmail.com>
2021-11-17 14:03:21 +0530

    Updates

---
+++ precache-manifest.fe12b14e55036b0c570449e3f1b99d70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78c097e41d9728ad269ee32519daf25",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "93fbde1bcec383662601",
+    "url": "/robofriends/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "d24ca32b6c768530819d",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "93fbde1bcec383662601",
+    "url": "/robofriends/static/js/2.08538456.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/robofriends/static/js/2.08538456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9511f4e9c8b09b9c5c5",
+    "url": "/robofriends/static/js/3.9ba2cfbb.chunk.js"
+  },
+  {
+    "revision": "d24ca32b6c768530819d",
+    "url": "/robofriends/static/js/main.b46b037a.chunk.js"
+  },
+  {
+    "revision": "003465745854db36decf",
+    "url": "/robofriends/static/js/runtime-main.4b1251ad.js"
+  }
+]);
